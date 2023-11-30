10896cc37dc09fe584e6b1088e7d655534ba418b
Pavithra-Paranthaman <pavithrap0530@gmail.com>
2023-11-30 18:01:33 -0500

    Fixed the substr and function declaration issue

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,18 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 let sortedNames = fruit.sort();
 const input = document.querySelector('#fruit');
+
+function useSuggestion(value) {
+	input.value = value;
+	removeElements();
+}
+function removeElements() {
+	//clear all the item
+	let items = document.querySelectorAll(".list-items");
+	items.forEach((item) => {
+		item.remove();
+	});
+}
 input.addEventListener("keyup", (e) => {
 	removeElements();
 	for (let i of sortedNames) {
@@ -20,8 +32,9 @@ input.addEventListener("keyup", (e) => {
 			listItem.style.cursor = "pointer";
 			listItem.setAttribute("onclick", "useSuggestion('" + i + "')");
 			//Display matched part in bold
-			let word = "<b>" + i.substr(0, input.value.length) + "</b>";
-			word += i.substr(input.value.length);
+			
+			let word = "<b>" + i.substring(0, input.value.length) + "</b>";
+			word += i.substring(input.value.length);
 			//display the value in array
 			listItem.innerHTML = word;
 			document.querySelector(".list").appendChild(listItem);
@@ -29,16 +42,6 @@ input.addEventListener("keyup", (e) => {
 	}
 });
 
-function useSuggestion(value) {
-	input.value = value;
-	removeElements();
-}
-function removeElements() {
-	//clear all the item
-	let items = document.querySelectorAll(".list-items");
-	items.forEach((item) => {
-		item.remove();
-	});
-}
+
 
 
