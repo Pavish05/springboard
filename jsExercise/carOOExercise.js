4a76af5107a90540e7ef89c4f975d39d47ebf347
Pavithra-Paranthaman <pavithrap0530@gmail.com>
2023-12-02 23:08:36 -0500

    Car- Object Oriented Programming Exercise

---
+++ jsExercise/carOOExercise.js
@@ -0,0 +1,48 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make;
+        this.model;
+        this.year;
+    }
+    honk() {
+        return ("Beep.");
+    }
+    toString() {
+        return `The vehicle is a $(this.make) $(this.model) $(this.year).`;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return ("VROOM!!!");
+    }
+}
+
+class Garage extends Vehicle {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle) {
+        if (!(newVehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
+}
